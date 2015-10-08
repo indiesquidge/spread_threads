c25930de6b5f1e19b2d9c48b27760f0317b702eb
Austin Wood <i@austinwood.me>
2015-10-07 22:47:33 -0600

    Create seed data task to use in development

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+class SeedItem
+  def self.create_items(number_of_items)
+    number_of_items.times do
+      item = Item.create!(title: Faker::App.name,
+                         blurb: Faker::Lorem.sentence(3),
+                         author: Faker::App.author,
+                         thumbnail_url: Faker::Avatar.image,
+                         details_url: Faker::Internet.url('example.com', '/foobar.html'))
+
+      puts "Created Item #{item.title}"
+    end
+  end
+end
+
+SeedItem.create_items(20)
