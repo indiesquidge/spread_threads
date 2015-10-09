5041b15fe8ca0b442774618813dcfb5ed60a9067
Austin Wood <i@austinwood.me>
2015-10-08 22:09:18 -0600

    All page params to be used in API index call

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    respond_with Item.all
+    respond_with Item.page(params[:page]).per(params[:per_page])
+  end
+
+  private
+
+  def page_params
+    params.permt(:page, :page_size)
   end
 end
