b9a9b80633616e84e47f37dfcde646baac9a34cb
Austin Wood <i@austinwood.me>
2015-10-08 21:25:28 -0600

    Update spec to test for pagination

--- spec/acceptance/api/v1/items_controller_spec.rb
+++ spec/acceptance/api/v1/items_controller_spec.rb
@@ -7,18 +7,27 @@ resource "Items" do
                               blurb: "for my importing/exporting business",
                               author: "Art Vandelay",) }
 
+  before :each do
+    19.times do
+      Item.create!(title: Faker::App.name,
+                   blurb: Faker::Lorem.sentence(3),
+                   author: Faker::App.author)
+    end
+  end
+
   let!(:item2) { Item.create!(title: "Mining Drill",
                               blurb: "for my silver mine in Peru",
                               author: "H.E. Pennypacker",) }
 
   context "#index" do
     get "/api/v1/items" do
-      it "Returns all items" do
-        do_request
+      parameter :page, "page scope"
+      let(:page) { 1 }
 
+      example_request "Returns 20 items per page specified" do
         expect(status).to eq 200
         expect(response_body).to include(item1.title)
-        expect(response_body).to include(item2.title)
+        expect(response_body).not_to include(item2.title)
       end
     end
   end
