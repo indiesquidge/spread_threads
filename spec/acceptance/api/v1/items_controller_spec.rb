18eeacc909917bc14101a36e3ad488ae33ed7ba2
Austin Wood <i@austinwood.me>
2015-10-08 22:34:42 -0600

    Include missing param for index route for docs

--- spec/acceptance/api/v1/items_controller_spec.rb
+++ spec/acceptance/api/v1/items_controller_spec.rb
@@ -8,7 +8,7 @@ resource "Items" do
                               author: "Art Vandelay",) }
 
   before :each do
-    19.times do
+    4.times do
       Item.create!(title: Faker::App.name,
                    blurb: Faker::Lorem.sentence(3),
                    author: Faker::App.author)
@@ -22,9 +22,11 @@ resource "Items" do
   context "#index" do
     get "/api/v1/items" do
       parameter :page, "page scope"
+      parameter :per_page, "items per page (default value is 20)"
       let(:page) { 1 }
+      let(:per_page) { 5 }
 
-      example_request "Returns 20 items per page specified" do
+      example_request "return a collection of items" do
         expect(status).to eq 200
         expect(response_body).to include(item1.title)
         expect(response_body).not_to include(item2.title)
