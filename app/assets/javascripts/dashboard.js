ed41070e7751e23f17e13145d91455e8e4129086
Austin Wood <i@austinwood.me>
2015-10-10 00:18:03 -0600

    Style dashboard and item cards

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -11,19 +11,33 @@ var ItemRepo = {
 function getItems() {
   ItemRepo.all().then(function (items) {
     var renderedItems = items.map(renderItem);
-    $('body').prepend(renderedItems);
+    $('.items').prepend(renderedItems);
   });
 }
 
 function renderItem(item) {
   var truncatedBlurb = trimBlurb(item.blurb);
 
-  return $('<div class="item" style="border: 2px solid;">' +
-           '<img src=' + item.thumbnail_url + '>' +
-           '<h3>' + item.title + '</h3>' +
-           '<h4>' + item.author + '</h4>' +
-           '<p>' + truncatedBlurb + '<a href=' + item.details_url + '>Read more</a>' + '</p>' +
-         '</div>');
+  return $('<div class="col s12 m3">' +
+             '<div class="card">' +
+               '<div class="card-image waves-effect waves-block waves-light">' +
+                 '<img class="activator" src=' + item.thumbnail_url + '>' +
+               '</div>' +
+               '<div class="card-content">' +
+                 '<span class="card-title activator grey-text text-darken-4">' +
+                   item.title + '<i class="material-icons right">more_vert</i>' +
+                 '</span>' +
+                 '<p><a href=' + item.details_url + '>Read more</a></p>' +
+               '</div>' +
+               '<div class="card-reveal">' +
+                 '<span class="card-title grey-text text-darken-4">' +
+                   item.title + '<i class="material-icons right">close</i>' +
+                 '</span>' +
+                 '<p>' + item.author + '</p>' +
+                 '<p>' + item.blurb + '</p>' +
+               '</div>' +
+             '</div>' +
+           '</div>');
 }
 
 function trimBlurb(string) {
