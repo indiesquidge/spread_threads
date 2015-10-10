748fbfe6eb24f6fd58d891a0f30b27d4e91ec3a1
Austin Wood <i@austinwood.me>
2015-10-10 02:40:19 -0600

    Update item card colors; remove unused trim function

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -16,35 +16,25 @@ function getItems() {
 }
 
 function renderItem(item) {
-  var truncatedBlurb = trimBlurb(item.blurb);
-
-  return $('<div class="col s12 m3">' +
-             '<div class="card">' +
+  return $('<div class="col s12 m6 l3">' +
+             '<div class="card grey lighten-5">' +
                '<div class="card-image waves-effect waves-block waves-light">' +
                  '<img class="activator" src=' + item.thumbnail_url + '>' +
                '</div>' +
-               '<div class="card-content">' +
+               '<div class="card-content white">' +
                  '<span class="card-title activator grey-text text-darken-4">' +
                    item.title + '<i class="material-icons right">more_vert</i>' +
                  '</span>' +
-                 '<p><a href=' + item.details_url + '>Read more</a></p>' +
                '</div>' +
-               '<div class="card-reveal">' +
-                 '<span class="card-title grey-text text-darken-4">' +
+               '<div class="card-reveal blue-grey">' +
+                 '<span class="card-title white-text">' +
                    item.title + '<i class="material-icons right">close</i>' +
                  '</span>' +
-                 '<p>' + item.author + '</p>' +
-                 '<p>' + item.blurb + '</p>' +
+                 '<span class="white-text">' +
+                   '<p>Creator: ' + item.author + '</p>' +
+                   '<p>' + item.blurb + '</p>' +
+                 '</span>' +
                '</div>' +
              '</div>' +
            '</div>');
-}
-
-function trimBlurb(string) {
-  var words = string.split(' ');
-
-  if (words.length > 32) {
-    return string.split(' ', 32).join(' ') + '...';
-  }
-  return string;
 }
