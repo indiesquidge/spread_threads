96a96e6265a13d28ce23811048d5ea58b14cdc97
Austin Wood <i@austinwood.me>
2015-10-11 02:30:44 -0600

    Add ability for infinite scroll via AJAX

---
+++ app/assets/javascripts/items.js
@@ -0,0 +1,8 @@
+$(function () {
+  return $('a.load-more-items').on('inview', function(e, visible) {
+    if (!visible) {
+      return;
+    }
+    return $.getScript($(this).attr('href'));
+  });
+});
